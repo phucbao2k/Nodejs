08c0075f54570c660acbb4312cd42375c5b79eea
phucbao2k <baophucta2k@gmail.com>
2022-12-24 20:37:57 +0700

    f

--- .history/src/models/index_20221223225032.js
+++ .history/src/models/index_20221223225032.js
@@ -7,7 +7,7 @@ const basename = path.basename(__filename);
 const env = process.env.NODE_ENV || 'development';
 // const config = require(__dirname + '/../config/config.json')[env];
 const db = {};
-//
+
 let sequelize;
 const customizeConfig = {
   host: process.env.DB_HOST,
